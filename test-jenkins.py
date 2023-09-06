c57dcb03381608e8087f5069939c8d3de0cfe9fe
thinkc <tunde.oyewo@gmail.com>
2023-09-06 16:36:26 +0100

    add python file7a

--- test-jenkins.py
+++ test-jenkins.py
@@ -1,9 +1,9 @@
 import subprocess
 
 commands = [
-     'curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -',
-     'sudo apt-get install -y nodejs',
-     'sudo apt-get install -y npm',
+    #  'curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -',
+    #  'sudo apt-get install -y nodejs',
+    #  'sudo apt-get install -y npm',
      'npm install',
      'npm test',
 ]
