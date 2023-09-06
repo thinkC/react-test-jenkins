5b12bbbb1f827ad97f35afef0f083102be261dd2
thinkc <tunde.oyewo@gmail.com>
2023-09-06 16:20:38 +0100

    add python file7

--- test-jenkins.py
+++ test-jenkins.py
@@ -4,8 +4,8 @@ commands = [
      'curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -',
      'sudo apt-get install -y nodejs',
      'sudo apt-get install -y npm',
-     'cd react-test-jenkins/ && npm install',
-     'cd react-test-jenkins/ && npm test',
+     'npm install',
+     'npm test',
 ]
 
 for command in commands:
